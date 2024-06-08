94b7ffa9526620a2e431fbd5f5882a613e7db4eb
rberaki <redae.beraki@gmail.com>
2024-06-07 19:52:25 -0500

    Cast config to int

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,6 @@ from decouple import config
 DB_URL = config('DATABASE_URL')
 SECRET_KEY = config('SECRET_KEY')
 ALGORITHM = config('ALGORITHM')
-TOKEN_EXPIRE_MINUTES = config('TOKEN_EXPIRE_MINUTES')
+TOKEN_EXPIRE_MINUTES = config('TOKEN_EXPIRE_MINUTES', cast=int)
 ISSUER = config("ISSUER")
 AUDIENCE = config("AUDIENCE")
